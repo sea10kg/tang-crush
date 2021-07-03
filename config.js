6a23bb60e887051e73a239341bd63154f3757d74
sea10kg <86876477+sea10kg@users.noreply.github.com>
2021-07-03 19:01:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hello',
+    introDesc: `Bạn ăn cơm chưa nhờ?
+    Ăn rồi thì đã húp canh chưa?`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'T có điều muốn nói',
+    desc: 'M nghe chứ?',
+    btnYes: 'Không nghe đâu',
+    btnNo: 'Ok, tớ nghe này',
+    question:'Trên thế giới có hơn 7 tỉ người, t chỉ nói với mỗi m, mà m lại không muốn nghe ư?',
+    btnReply: 'Gửi cho bạn',
+    reply: 'Đúng vậy, không muốn nghe',
+    mess: 'T biết mà. M ghét t đúng không?',
+    messDesc: 'Trước giờ t toàn bắt nạt m, nên m ghét t đúng không',
+    btnAccept: 'Hiểu được là tốt rồi',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
